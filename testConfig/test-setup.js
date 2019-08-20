ae155f9da142072ec77ab365dcbe36f1fafd9133
jktravis <joshua.travis@gmail.com>
2019-08-20 09:40:19 -0400

    remove unused deps

--- testConfig/test-setup.js
+++ testConfig/test-setup.js
@@ -1,14 +1,3 @@
-/**
- * Defines the React 16 Adapter for Enzyme.
- *
- * @link http://airbnb.io/enzyme/docs/installation/#working-with-react-16
- * @copyright 2017 Airbnb, Inc.
- */
-const enzyme = require("enzyme");
-const Adapter = require("enzyme-adapter-react-16");
-
-enzyme.configure({ adapter: new Adapter() });
-
 process.on("unhandledRejection", (reason, p) => {
   console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
   // application specific logging, throwing an error, or other logic here
